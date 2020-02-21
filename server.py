a4ce943638b65fed91252b7dc50f4d1d44b9d1b2
Raj Vishwakarma <rajvishwakarma@Rajs-MacBook-Air.local>
2020-02-21 22:41:56 +0530

    inital commit

---
+++ server.py
@@ -0,0 +1,58 @@
+import psycopg2
+from flask import Flask
+from flask import jsonify
+import json
+import pandas as pd
+
+app = Flask(__name__)
+
+
+fire = "select * from emp"
+
+
+
+def emp():
+    try:
+        connection = psycopg2.connect(user = "postgres",
+                                    password = "9159",
+                                    host = "127.0.0.1",
+                                    port = "5432",
+                                    database = "manager")
+
+        # cursor = connection.cursor()
+        # Print PostgreSQL Connection properties
+        # print ( connection.get_dsn_parameters(),"\n")
+
+        # Print PostgreSQL version
+        # cursor.execute("select * from emp;")
+        # record = cursor.fetchall()
+        record = pd.read_sql(fire,connection)
+        #print("You are connected to - ", record,"\n")
+        #print(type(record))
+
+        return record
+
+    except (Exception, psycopg2.Error) as error :
+        print ("Error while connecting to PostgreSQL", error)
+    finally:
+        #closing database connection.
+            if(connection):
+                # cursor.close()
+                connection.close()
+                # print("PostgreSQL connection is closed")
+
+
+
+@app.route('/emp')
+def home():
+    return "Helloworld"
+    # data = emp()
+    # print(data)
+    # return json.dumps(data)
+    # return jsonify("hello")
+    #return json.dumps(data.to_dict(orient='records'))
+    # return jsonify(data.to_dict(orient='records'))
+    #return 'Hello Flask'
+
+#home()
+# app.run(debug=True)
